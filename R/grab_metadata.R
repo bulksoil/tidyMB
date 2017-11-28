9e22da4af4908fd2e632adccd87f592d8ff1253c
Joseph Edwards <edwards@ucdavis.edu>
2017-11-28 12:37:21 -0800

    Updating

--- R/grab_metadata.R
+++ R/grab_metadata.R
@@ -11,7 +11,7 @@
 grab_metadata <- function(x, samples = "SampleID", otus = "variable"){
 	message("Gathering metadata")
 	to_drop <- otus
-	warning(paste("Removing the column ", to_drop, " from the table.\n", sep = ""))
+	warning(paste("Removing the column ", to_drop, " from the metadata.\n", sep = ""))
  	metadata <- x %>% 
 		dplyr::ungroup() %>% 
 		purrr::discard(is.double) %>% 

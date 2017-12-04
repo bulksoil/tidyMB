dad4bc38e76548833be02ba5450ca1ac5ac35b88
Joseph Edwards <edwards@ucdavis.edu>
2017-12-04 12:44:45 -0800

    Added function for capscale

--- R/tidy_cap.R
+++ R/tidy_cap.R
@@ -1,26 +1,25 @@
-#' PCoA on Long Data
+#' Constrained Ordination on Tidy Data
 #'
 #' This function is a wrapper around Vegan's capscale function for performing PCoA on tidy (long) data.
 #' @param samples The column header for your sample identifiers. Defaults to SampleID
 #' @param otus The column header for your OTU identifiers. Defaults to variable
 #' @param value The column header for the OTU abundances. Defaults to RA
 #' @param dist The distance metric options from vegan's vegdist(). Defaults to bray
+#' @param formula The formula to feed into the capscale function
 #' @return A list containing the axes, the eigenvalues, and loadings (optional)
 #' @keywords PCoA
 #' @export
 #' @examples
-#' tidy_pcoa()
+#' tidy_cap()
 
-tidy_pcoa <- function(x, samples = "SampleID", otus = "variable", value = "RA", dist = "bray", keep_loadings = F){
+tidy_cap <- function(x, samples = "SampleID", otus = "variable", value = "RA", dist = "bray", formula = NULL){
 
-  metadata <- tidyMB::grab_metadata(x, samples = samples, otus = otus)
-  wide_table <- tidyMB::widen(x, samples = samples, otus = otus, value = value)
+  metadata <- tidyMB::grab_metadata(x, samples = samples, otus = otus, return_df = F)
+  wide_table <- tidyMB::widen(x, samples = samples, otus = otus, value = value, return_df = F)
   
-  pc <- vegan::capscale(wide_table[,2:ncol(wide_table)] ~ 1, dist = dist)
+  pc <- vegan::capscale(as.formula(paste("wide_table[,2:ncol(wide_table)] ~ ", formula, sep = "")), data = metadata, dist = dist)
   axes <- dplyr::bind_cols(metadata, dplyr::as_tibble(vegan::scores(pc, choices = c(1:5))$sites))
   eigen_vals <- vegan::eigenvals(pc) / sum(vegan::eigenvals(pc))
 
-  ifelse(keep_loadings, 
-    return(list(axes = axes, loadings = pc$CA$v, eigen_vals = eigen_vals)),
-    return(list(axes = axes, eigen_vals = eigen_vals)))
+  return(list(axes = axes, eigen_vals = eigen_vals))
 }

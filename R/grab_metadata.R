f54191b556425a146d46d2af2227f18c89f24f42
bulksoil <edwards@ucdavis.edu>
2018-08-16 16:41:36 -0500

    Fixing integer bug

--- R/grab_metadata.R
+++ R/grab_metadata.R
@@ -16,6 +16,7 @@ grab_metadata <- function(x, samples = "SampleID", otus = "variable", return_df 
  	metadata <- x %>% 
 		dplyr::ungroup() %>% 
 		purrr::discard(is.double) %>% 
+		purr::discard(is.integer) %>%
 		dplyr::select_(.dots = paste("-", to_drop)) %>% 
 		dplyr::distinct()
 

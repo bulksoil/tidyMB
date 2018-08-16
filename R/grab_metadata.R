ffd38813aa60265357912bd2af2a60bba3d37d9e
bulksoil <edwards@ucdavis.edu>
2018-08-16 16:59:30 -0500

    Fixed a typo

--- R/grab_metadata.R
+++ R/grab_metadata.R
@@ -16,7 +16,7 @@ grab_metadata <- function(x, samples = "SampleID", otus = "variable", return_df 
  	metadata <- x %>% 
 		dplyr::ungroup() %>% 
 		purrr::discard(is.double) %>% 
-		purr::discard(is.integer) %>%
+		purrr::discard(is.integer) %>%
 		dplyr::select_(.dots = paste("-", to_drop)) %>% 
 		dplyr::distinct()
 
